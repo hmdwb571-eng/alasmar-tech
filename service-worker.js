05c8a366cb18705f573ea32da243fb95ac11181f
hmdwb571-eng <hmdwb571@gmail.com>
2026-07-28 20:02:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,37 @@
-const cacheName = "alasmar-tech-v2";
+const cacheName = "alasmar-tech-v4";
 
 const filesToCache = [
   "index.html",
   "style.css",
   "manifest.json",
-
-  "images/logo.png"
-
+  "icons/logo.png"
 ];
 
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
+    }).then(function() {
+      return self.skipWaiting();
     })
   );
 });
 
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
 
 self.addEventListener("fetch", function(event) {
   event.respondWith(
